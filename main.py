dccd5ba12a675e42f602d8ece4b9a5e5186e4feb
Quazi Md Aldin Fardin <59772487+programmer-Quazi@users.noreply.github.com>
2022-06-06 13:03:12 +0000

    exercise 03

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("py")
+# print("py")
+
+'''
+
+String Menipulation And Code Intelligence
+
+'''
+
+
+# print("hello world! \nHello World  ")
+
+
+# print ("hellow" + "Tara")
+# print ("hellow" + " Tara")
+# print("Hello" + " " + "Tara")
+
+
+'''
+
+input function
+
+
+'''
+# input("What is your name?")
+print("Hello" + input("What is yourt name?"))
+
+
+
+
