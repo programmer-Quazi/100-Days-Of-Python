e09f01acb9352738b6c6448596612111b3e89057
programmer-Quazi <kferdoush617@gmail.com>
2022-06-13 00:27:11 +0600

    3rd day updated

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("py")
+print("Welcome to the rollercoster")
+height = int(input("How tall is the rollercoaster? "))
+
+
+if height >= 120:
+    print("You can ride  rollercoaster")
+    age = int(input("How old are you? "))
+    if age <= 18:
+        print("Please Pay $7")
+    else:
+        print("Please Pay !2$  ")
+else:
+    print("Sorry, You have to grow taller before you can ride")
+
+
+
+# TODO:Nested if / else 
+
